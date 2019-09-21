3f26c1b97abed0d1851319d9e2943891acbe6573
henrylee2cn <henrylee2cn@gmail.com>
2019-09-21 15:02:54 +0800

    feat: release
    Change-Id: Ifd3b18bda2963393430249f0ac543ea9d4e1f21f

---
+++ chinaid_test.go
@@ -0,0 +1,78 @@
+package chinaid
+
+import (
+	"testing"
+)
+
+func Test18(t *testing.T) {
+	// make ChinaID18
+
+	addr := [6]byte{}
+	for k, v := range "326221" {
+		addr[k] = byte(v)
+	}
+	birth := [8]byte{}
+	for k, v := range "19880102" {
+		birth[k] = byte(v)
+	}
+	seq := [3]byte{}
+	for k, v := range "124" {
+		seq[k] = byte(v)
+	}
+	id18, ok := Make18(addr, birth, seq)
+	t.Logf("Make18(%v): %s", ok, id18)
+
+	// check ChinaID18
+	chinaID, err := Parse(id18.String())
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	t.Logf("id18 format(%%v): %v, sex: %s\n\n", chinaID, chinaID.SexText())
+	t.Logf("id18 format(%%+v): %#v\n\n", chinaID)
+}
+
+func Test15(t *testing.T) {
+	// make ChinaID15
+
+	addr := [6]byte{}
+	for k, v := range "326221" {
+		addr[k] = byte(v)
+	}
+	birth := [6]byte{}
+	for k, v := range "880102" {
+		birth[k] = byte(v)
+	}
+	seq := [3]byte{}
+	for k, v := range "124" {
+		seq[k] = byte(v)
+	}
+	id15, ok := Make15(addr, birth, seq)
+	t.Logf("Make15(%v): %s", ok, id15)
+
+	// check ChinaID15
+	chinaID, err := Parse(id15.String())
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	t.Logf("id15 format(%%v): %v, sex: %s\n\n", chinaID, chinaID.SexText())
+	t.Logf("id15 format(%%+v): %#v\n\n", chinaID)
+}
+
+func TestValidate(t *testing.T) {
+	ok := Validate("32622119880102124X")
+	if !ok {
+		t.Fail()
+	}
+}
+
+func TestCheckCode(t *testing.T) {
+	checkCode, ok := CheckCode("32622119880102124")
+	if !ok || checkCode != "X" {
+		t.Fatalf("get '%s', expect 'X'", checkCode)
+	}
+
+	checkCode, ok = CheckCode("326221880102124")
+	if ok {
+		t.Fatalf("get '%s', expect ''", checkCode)
+	}
+}
